834e77dbe854ea297dbd04c4f2f3c8b74a79d19a
Hugo L. Ávila <53014804+avilaHugo@users.noreply.github.com>
2024-01-23 20:47:53 -0300

    Add ID:RNA

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fs;
+use std::path::PathBuf;
 use std::collections::HashMap;
 use itertools::Itertools;
 
@@ -29,6 +31,23 @@ impl Seq {
     }
 }
 
+pub fn counting_dna_nucleotides(input_file: PathBuf) {
+    let seq = fs::read_to_string(input_file).expect("File not found");
+    let kmer_counter = Seq::from_str(seq).count_kmers(1);
+    let result = ["A", "C", "G", "T"]
+        .map(|x| kmer_counter.get(x).unwrap().to_string())
+        .join(" ");
+
+    println!("{}", result)
+}
+
+pub fn transcribing_dna_into_rna(input_file: PathBuf) {
+	let content = fs::read_to_string(input_file).expect("File not found !.");
+	let seq = Seq::from_str(content);
+	let result = seq.seq.replace("T", "U");
+	println!("{}", result)
+}
+
 #[cfg(test)]
 mod tests {
     use crate::Seq;
@@ -38,5 +57,12 @@ mod tests {
 	let seq = Seq::from_str("ACTG".to_string());
 	let kmer_counter = seq.count_kmers(1);
 	assert_eq!(1, *kmer_counter.get("A").unwrap())
+    }
+
+    #[test]
+    fn test_transcribing_dna_into_rna(){
+	let seq = Seq::from_str("GATGGAACTTGACTACGTAAATT".to_string());
+	let result = seq.seq.replace("T", "U");
+	assert_eq!("GAUGGAACUUGACUACGUAAAUU", result)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use rosalind;
-use std::fs;
-use std::path::PathBuf;
+use rosalind::*;
 use structopt::StructOpt;
+use std::path::PathBuf;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "rosalind", about = "Project Rosalind problem solver.")]
@@ -15,20 +14,13 @@ struct Opt {
     input_file: PathBuf,
 }
 
-fn counting_dna_nucleotides(input_file: PathBuf) {
-    let seq = fs::read_to_string(input_file).expect("File not found");
-    let kmer_counter = rosalind::Seq::from_str(seq).count_kmers(1);
-    let result = ["A", "C", "G", "T"]
-        .map(|x| kmer_counter.get(x).unwrap().to_string())
-        .join(" ");
-
-    println!("{}", result)
-}
 
 fn main() {
     let opt = Opt::from_args();
     match opt.problem_id.as_str() {
         "DNA" => counting_dna_nucleotides(opt.input_file),
+	"RNA" => transcribing_dna_into_rna(opt.input_file),
         _ => panic!("Problem ID unknown"),
+
     }
 }
